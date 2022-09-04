89f94c55690972b0fae83e0b52308a96533df9f9
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2022-09-04 02:20:33 -0500

    added message resource: migration, model, test

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   validates :username, presence: true, length: { minimum: 3, maximum: 15 }
   validates_uniqueness_of :username
+  has_many :messages
   has_secure_password
 end
