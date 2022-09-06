f654ce78f9ceb6f6839a804da3169f25eaa7968b
Ricardo Ruíz <rich.ruiz.v@gmail.com>
2022-09-06 01:04:04 -0500

    creation and setup of actionCable

--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,6 @@ Rails.application.routes.draw do
   post 'login', to: 'session#create'
   delete 'logout', to: 'session#destroy'
   post 'message', to: 'messages#create'
+
+  mount ActionCable.server, at:  '/cable'
 end
